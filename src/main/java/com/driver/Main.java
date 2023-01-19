2bad4f9725d6a3193405f8cc2e634d88b9abb151
krishna479 <krishankant.sharma@gmail.com>
2023-01-19 22:30:53 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
-
+import java.util.*;
+import java.io.*;
+import java.lang.*;
 public class Main {
-  
+    public static void main(String[] args) {
+     RWOnly obj = new RWOnly();
+       // obj.name ="jits";
+        // name has private access in com.driver.RWOnly
+        obj.setName("jits");
+        System.out.println(obj.getName());
+
+    }
 }
